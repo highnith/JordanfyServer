8791b6e7f1dfca7ae269afff007814c6ee448ad8
Giorgio <giorgiosisti12@gmail.com>
2026-05-05 21:40:49 +0200

    readdes cookies

--- main.py
+++ main.py
@@ -66,11 +66,10 @@ def download_audio(video_id: str):
         ydl_opts = {
             "format": "bestaudio[ext=m4a]/bestaudio/best",
             "outtmpl": str(DOWNLOAD_DIR / "%(id)s.%(ext)s"),
-            "extractor_args": {
-                "youtube": {
-                    "player_client": ["android_vr", "web"],
-                }
-            },
+            "cookiefile": "cookies.txt",
+            "http_headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    },
             "noplaylist": True,
             "quiet": True,
             "postprocessors": [{
